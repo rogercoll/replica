f7d6d6af7d74b1b00fa30c12434374caa906e5c0
Roger Coll <rogercoll@protonmail.com>
2021-07-05 18:47:11 +0200

    Support multiple local replications

--- plugins/distributors/local/local.go
+++ plugins/distributors/local/local.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"path"
+	"sync"
 
 	"github.com/rogercoll/replica"
 	"github.com/rogercoll/replica/plugins/distributors"
@@ -15,14 +16,14 @@ var (
 )
 
 type Local struct {
-	Destination string `toml:"destination"`
+	Destinations []string `toml:"destinations"`
 }
 
 const sampleConfig = `
-  ## Store files in a local path
-  # Absolute direcotry path to store the backup files
+  ## Copy backup files to other directories
+  # Absolute direcotries path to store the backup files
   # Default is $HOME/backups
-  destination = "" 
+  destination = [] 
 `
 
 // SampleConfig returns a sample TOML section to illustrate configuration
@@ -32,33 +33,41 @@ func (l *Local) SampleConfig() string {
 }
 
 func (l *Local) Description() string {
-	return "Store backupfiles to a local directory"
+	return "Store backupfiles to a local directories"
 }
 
 func (l *Local) Save(files []string) (int64, error) {
 	var total int64
 	for _, file := range files {
-		newFile, err := os.Create(l.Destination + path.Base(file))
-		if err != nil {
-			log.Fatal(err)
+		var wg sync.WaitGroup
+		for _, localDir := range l.Destinations {
+			wg.Add(1)
+			go func(ldir string) {
+				defer wg.Done()
+				newFile, err := os.Create(ldir + path.Base(file))
+				if err != nil {
+					log.Fatal(err)
+				}
+				defer newFile.Close()
+				oldFile, err := os.Open(file)
+				if err != nil {
+					log.Fatal(err)
+				}
+				defer oldFile.Close()
+				bytesCopied, err := io.Copy(newFile, oldFile)
+				if err != nil {
+					log.Fatal(err)
+				}
+				total += bytesCopied
+			}(localDir)
 		}
-		defer newFile.Close()
-		oldFile, err := os.Open(file)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer oldFile.Close()
-		bytesCopied, err := io.Copy(newFile, oldFile)
-		if err != nil {
-			log.Fatal(err)
-		}
-		total += bytesCopied
+		wg.Wait()
 	}
 	return total, nil
 }
 
 func init() {
 	distributors.Add("local", func() replica.Distributor {
-		return &Local{Destination: defaultDestination}
+		return &Local{Destinations: []string{defaultDestination}}
 	})
 }
